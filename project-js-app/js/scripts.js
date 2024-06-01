8d7f3c1fcdaa20ba6dfc145ae09a6a96a67a7422
TBlunck1 <132540089+TBlunck1@users.noreply.github.com>
2024-06-01 15:34:38 -0600

    Update to project-js-app
    Added a pokemonlist to create a pokedex for Pokemon players

--- project-js-app/js/scripts.js
+++ project-js-app/js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+    {name: 'Jigglypuff', height: 0.5, type: ['Fairy', 'normal']},
+    {name: 'Charmander', height: 0.6, type: 'Fire'},
+];

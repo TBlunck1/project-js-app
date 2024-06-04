505aebc92253d6765ce9bc77fa978e243229274f
TBlunck1 <132540089+TBlunck1@users.noreply.github.com>
2024-06-03 21:13:12 -0600

    Update scripts.js
    added a conditional (for) to the pokemon list to find the tallest pokemon and say "Wow, that's a big pokemon".

--- project-js-app/js/scripts.js
+++ project-js-app/js/scripts.js
@@ -1,5 +1,13 @@
 let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-    {name: 'Jigglypuff', height: 0.5, type: ['Fairy', 'normal']},
-    {name: 'Charmander', height: 0.6, type: 'Fire'},
+    {name: 'Charzard', height: 1.7, type: ['Fire', 'Flying']},
+    {name: 'Drowzee', height: 1.0, type: 'Psychic'},
 ];
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height <1.1){
+        console.log(pokemonList[i].height)
+    }else if (pokemonList[i].height >1.1){
+        console.log(pokemonList[i].height + " Wow, that's a big pokemon!!")
+    }
+}

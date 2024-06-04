0161262eecb0ef84751bd9fc669a7e879dc6c96a
TBlunck1 <132540089+TBlunck1@users.noreply.github.com>
2024-06-03 21:19:24 -0600

    Update script.js
    just leaving comments to let others know what this part of the code is doing

--- project-js-app/js/scripts.js
+++ project-js-app/js/scripts.js
@@ -5,9 +5,12 @@ let pokemonList = [
 ];
 
 for (let i=0; i < pokemonList.length; i++){
+    //this is a conditional for to say what to print to the console when looking up a certain pokemon height
     if (pokemonList[i].height <1.1){
         document.write(pokemonList[i].height)
+        //this will only name the pokemons name and height if it is less than 1.1
     }else if (pokemonList[i].height >1.1){
         document.write(pokemonList[i].height + " Wow, that's a big pokemon!!")
+        //this will say if the pokemon's height is greater than 1.1 m it will say "Wow, that's a big pokemon"
     }
 }

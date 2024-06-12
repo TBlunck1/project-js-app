156a5c59f55396855eb6b033b32a7284caf3330a
TBlunck1 <132540089+TBlunck1@users.noreply.github.com>
2024-06-12 12:33:41 -0600

    Debugging errors in JavaScript
    Not linking JavaScript file to HTML

--- project-js-app/js/scripts.js
+++ project-js-app/js/scripts.js
@@ -1,16 +1,23 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
-    {name: 'Charzard', height: 1.7, type: ['Fire', 'Flying']},
-    {name: 'Drowzee', height: 1.0, type: 'Psychic'},
+    {id: 1, name:'Bulbasaur', height: 0.7, type: ['Grass', 'Poison']},
+    {id: 2, name: 'Charzard', height: 1.7, type: ['Fire', 'Flying']},
+    {id: 3, name: 'Squirtile', height: 0.5, type: 'Water'}
 ];
 
-for (let i=0; i < pokemonList.height; i++){
-    //this is a conditional for to say what to print to the console when looking up a certain pokemon height
-    if (pokemonList[i].height <1.1){
-        document.write(pokemonList[i].height)
-        //this will only name the pokemons name and height if it is less than 1.1
-    }else if (pokemonList[i].height >1.1){
-        document.write(pokemonList[i].height + " Wow, that's a big pokemon!!")
-        //this will say if the pokemon's height is greater than 1.1 m it will say "Wow, that's a big pokemon"
+// array of all of my pokemon I have listed. The array's name is (pokemonList)
+
+for (let i=0; i < pokemonList.length; i++) {
+    //this is looping through the pokemonList
+    let pokemon = pokemonList[i];
+    //Declaring varibale with pokemon
+    document.write(pokemon.name + '(height: ' + pokemon.height + ')');
+    //Writing the pokemon name and height on the websites DOM
+    if (pokemon.height > 1) {
+        document.write(' - Wow, that\'s big!');
+        //this conditional is looking for pokemon that is greater than 1 and saying "Wow, that's big!"
     }
+    document.write('</br>');
+    //creating a line break in the websites DOM with HTML
 }
+
+//this is a for loop that is looping through all the pokemon in the array (pokemonList)

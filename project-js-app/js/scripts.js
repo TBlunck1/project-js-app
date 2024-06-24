e202604ff39fc5ce02d39aea44b9ea00069e7fcc
TBlunck1 <taylorbk99@gmail.com>
2024-06-24 16:48:39 -0600

    Update to script.js file
    Created a object with a list of pokemon, also added a forEach loop instead of a for loop, with functions with getAll and return. Also added a filter function in as well

--- project-js-app/js/scripts.js
+++ project-js-app/js/scripts.js
@@ -1,23 +1,85 @@
-let pokemonList = [
-    {id: 1, name: 'Blubasaur', height: 0.7, type: ['Grass', 'Poison']},
-    {id: 2, name: 'Charzard', height: 1.7, type: ['Fire', 'Flying']},
-    {id: 3, name: 'Squirtile', height: 0.5, type: 'Water'}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [];
+    pokemonList = [
+        {id: 1, name: 'Blubasaur', height: 0.7, type: ['Grass', 'Poison']},
+        {id: 2, name: 'Charzard', height: 1.7, type: ['Fire', 'Flying']},
+        {id: 3, name: 'Squirtile', height: 0.5, type: 'Water'},
+        {id: 4, name: 'Pidgey', height: 1, type: ['Normal', 'Flying']},
+        {id: 5, name: 'Pikachu', height: 0.9, type: 'Electric'},
+        {id: 6, name: 'Clefairy', height: 1.5, type: 'Fairy'},
+        {id: 7, name: 'Zubat', height: 0.5, type: ['poison', 'Flying']},
+        {id: 8, name: 'Psyduck', height: 1.5, type: 'Water'},
+        {id: 9, name: 'Abra', height: 1.2, type: 'Psychic'},
+        {id: 10, name: 'Machop', height: 1.5, type: 'Fighting'}
+    ];
 
-// array of all of my pokemon I have listed. The array's name is (pokemonList)
-
-for (let i=0; i < pokemonList.length; i++) {
-    //this is looping through the pokemonList
-    let pokemon = pokemonList[i];
-    //Declaring varibale with pokemon
-    document.write(pokemon.name + '(height: ' + pokemon.height + ')');
-    //Writing the pokemon name and height on the websites DOM
-    if (pokemon.height > 1) {
-        document.write(' - Wow, that\'s big!');
-        //this conditional is looking for pokemon that is greater than 1 and saying Wow, that's big!
+    function getAll() {
+        return pokemonList;
     }
-    document.write('</br>');
-    //creating a line break in the websites DOM with HTML
+
+    // This adds a Pokemon to the list
+    function add(pokemon) {
+        let keys = checkKeys(pokemon);
+        let lenght = Object.keys(pokemon).length; // Check lenght of the object
+        // Checks to see if argument is an object 
+        if (typeof pokemon !== 'object') {
+            console.log('Pokemon is not an Object!');
+        } else if (keys === lenght) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('Wrong Keys');
+        }
+    }
+
+    // This checks if the object has the right keys
+    function checkKeys(pokemon) {
+        let i = 0;
+        Object.keys(pokemon).forEach(function (key) {
+            if (key == 'name' || key == 'height' || key == 'types') {
+                i++;
+            } else {
+                i--;
+            }
+        });
+        return i;
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+    };
+})();
+
+// Print all Pokemons + height
+function printPokemonList(pokemon) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
+    if (pokemon.height > 1.7) {
+        document.write("Wow, that's big!" + '<br>');
+    } else {
+        document.write('<br>');
+    }
 }
 
-//this is a for loop that is looping through all the pokemon in the array (pokemonList)
+// Log Pokemons to console
+function logPokemonList(pokemon) {
+    console.log(pokemon.name + ' (height: ' + pokemon.height + ') ');
+}
+
+// Filter Pokemons by name
+function filterByName(pokemonName) {
+    return pokemonRepository
+        .getAll()
+        .filter((name) => name.name.includes(pokemonName));
+}
+
+// Log pokemonRepository
+console.log(pokemonRepository.getAll());
+
+// Add a new Pokemon
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['Electric'] });
+
+// Print all Pokemons
+pokemonRepository.getAll().forEach(printPokemonList);
+
+// Log filtered Pokemons
+filterByName('u').forEach(logPokemonList);
